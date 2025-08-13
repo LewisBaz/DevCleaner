45aadf6fea06f03f9482ba900706352990ab6596
Lev Bazhkov <lev.bazhkov@domrf.ru>
2025-08-13 18:28:52 +0500

    proj init

---
+++ DevCleaner/DevCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevCleanerApp.swift
+//  DevCleaner
+//
+//  Created by Lewis on 13.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct DevCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
